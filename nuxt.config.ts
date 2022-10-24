f56c5844b9cdfd257978f854c1db8dba52c9e84a
bertalanV <bertalan.verebelyi@gmail.com>
2022-10-24 22:24:47 +0200

    feat: basics
    add basic stuffs to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/_colors.scss" as *;',
+        },
+      },
+    },
+  },
 });
