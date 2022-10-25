ab1b5e83dfae6eb32722ca93bee834b663079810
bertalanV <bertalan.verebelyi@gmail.com>
2022-10-25 22:11:13 +0200

    feat: encounter
    - add encounter interface
    - add layout
    - add menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <AppHeader />
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+          <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
     <slot />
-    <AppFooter />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+import type { Encounter as EncounterInterface } from "~~/interfaces/Encounter";
+
+const model: EncounterInterface = {
+  name: "Mephisto",
+  image: "image.png",
+  count: 3,
+};
+</script>
+
 <template>
   <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert> This is an auto-imported component </AppAlert>
+    <h1>Encounters</h1>
+    <encounter :encounter="model" />
   </div>
 </template>
